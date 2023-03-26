3b466205f82f16ad0e068cc08281e9bb5514c241
Jan Kędzierski <jkedzierski087@gmail.com>
2023-03-26 18:11:50 +0200

    add post function 'addPostForm'

--- src/components/pages/PostAdd/PostAdd.js
+++ src/components/pages/PostAdd/PostAdd.js
@@ -1,7 +1,10 @@
+import AddPostForm from "../../Features/AddPostForm";
+
 const PostAdd = () => {
   return (
     <div>
       <h1>PostAdd</h1>
+      <AddPostForm />
     </div>
   );
 };
--- src/redux/postsRedux.js
+++ src/redux/postsRedux.js
@@ -5,16 +5,21 @@ export const getAllPosts = ({ posts }) => posts;
 const createActionName = actionName => `app/posts/${actionName}`;
 const GENERATE_POSTS = createActionName('GENERATE_POSTS');
 const REMOVE_CARD = createActionName('REMOVE_CARD');
+const ADD_POST = createActionName('ADD_POST');
 
 //action creators
 export const generatePosts = payload => ({ type: GENERATE_POSTS, payload,});
 export const removeCard = payload => ({ type: REMOVE_CARD, payload,});
+export const addPost = payload => ({ type: ADD_POST, payload,});
+
 const postsRecuder = (statePart = [], action) => {
   switch (action.type) {
     case GENERATE_POSTS:
       return [...statePart, ...action.payload];
     case REMOVE_CARD:
       return statePart.filter((post) => post.id !== action.payload);
+    case ADD_POST:
+      return [...statePart, { ...action.payload }]; 
     default:
       return statePart;
   }
