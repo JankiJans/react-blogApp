efc3163bf2d0a02dd0d73e45b5fd8403b07fa093
Jan Kędzierski <jkedzierski087@gmail.com>
2023-03-31 20:14:46 +0200

    modify categories still error

--- src/components/pages/Categories/Categories.js
+++ src/components/pages/Categories/Categories.js
@@ -1,23 +1,45 @@
-import {ListGroup, Col} from 'react-bootstrap';
+import { useState } from 'react';
+import { ListGroup, Col, Row } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 import { getCategories } from '../../../redux/categoriesRedux';
+import { useParams } from 'react-router-dom';
+import { postsByCategory } from '../../../redux/categoriesRedux';
+import PostList from '../../Features/PostList';
 
 const CategoryPage = () => {
+  const { category } = useParams();
+  const [selectedCategory, setSelectedCategory] = useState(category);
+  const categories = useSelector(getCategories);
+  const posts = useSelector((state) => postsByCategory(state, selectedCategory));
 
-    const categories = useSelector(getCategories);
+  const handleCategoryClick = (categoryItem) => {
+    setSelectedCategory(categoryItem);
+  };
 
   return (
     <div>
-        <h1>Categories</h1>
-        <ListGroup>
-            <Col xs={12} md={6} lg={4}>
-                <ListGroup>
-                    {categories.map((category) => (
-                        <ListGroup.Item key={category}><a href={'/categories/' + category}>{category}</a></ListGroup.Item>
-                    ))}
-                </ListGroup>
-            </Col>
-        </ListGroup>
+      <h1>Categories</h1>
+      <Row>
+        <Col xs={6} md={6} lg={6}>
+          <ListGroup>
+            {categories.map((categoryItem) => (
+              <ListGroup.Item key={categoryItem}>
+                <a href="#" onClick={() => handleCategoryClick(categoryItem)}>
+                  {categoryItem}
+                </a>
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+        </Col>
+        <Col xs={6} md={6} lg={6}>
+          <h2>Selected Category: {selectedCategory}</h2>
+          <ul>
+            {posts.map((post) => (
+              <PostList title={post.title} author={post.author} publishedDate={post.publishedDate} content={post.content} category={post.category}></PostList>
+            ))}
+          </ul>
+        </Col>
+      </Row>
     </div>
   );
 };
--- src/redux/categoriesRedux.js
+++ src/redux/categoriesRedux.js
@@ -1,4 +1,5 @@
 export const getCategories = (state) => state.categories;
+export const postsByCategory = (state, category) => state.posts.filter((post) => post.category === category);
 
 const categoryReducer = (statePart = [], action) => {
   switch (action.type) {
