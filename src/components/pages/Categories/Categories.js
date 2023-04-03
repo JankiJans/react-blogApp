ba3dacc01713d106ddf2ad85b9cd4bd59dec94c2
Jan Kędzierski <jkedzierski087@gmail.com>
2023-04-03 19:53:35 +0200

    categories page now working (problems with styles

--- src/components/pages/Categories/Categories.js
+++ src/components/pages/Categories/Categories.js
@@ -3,14 +3,12 @@ import { ListGroup, Col, Row } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 import { getCategories } from '../../../redux/categoriesRedux';
 import { useParams } from 'react-router-dom';
-import { postsByCategory } from '../../../redux/categoriesRedux';
 import PostList from '../../Features/PostList';
 
 const CategoryPage = () => {
   const { category } = useParams();
   const [selectedCategory, setSelectedCategory] = useState(category);
   const categories = useSelector(getCategories);
-  const posts = useSelector((state) => postsByCategory(state, selectedCategory));
 
   const handleCategoryClick = (categoryItem) => {
     setSelectedCategory(categoryItem);
@@ -18,9 +16,12 @@ const CategoryPage = () => {
 
   return (
     <div>
-      <h1>Categories</h1>
+      <header className="d-flex justify-content-between">
+        <h1>Categories</h1>
+        <h1>Selected Category: {selectedCategory}</h1>
+      </header>
       <Row>
-        <Col xs={6} md={6} lg={6}>
+        <Col>
           <ListGroup>
             {categories.map((categoryItem) => (
               <ListGroup.Item key={categoryItem}>
@@ -32,12 +33,9 @@ const CategoryPage = () => {
           </ListGroup>
         </Col>
         <Col xs={6} md={6} lg={6}>
-          <h2>Selected Category: {selectedCategory}</h2>
-          <ul>
-            {posts.map((post) => (
-              <PostList title={post.title} author={post.author} publishedDate={post.publishedDate} content={post.content} category={post.category}></PostList>
-            ))}
-          </ul>
+          <div> 
+            <PostList category={selectedCategory} />
+          </div>
         </Col>
       </Row>
     </div>
